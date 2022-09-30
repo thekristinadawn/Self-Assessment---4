816a84d4cd3df295988fe0ffae17f585e3281d2e
Kristina Palmer <thekristinadawn@gmail.com>
2022-09-30 09:59:16 -0500

    2nd Commit

--- Self-Assessment---4/SelfAssessment.cs
+++ Self-Assessment---4/SelfAssessment.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Linq;
 
 namespace Self_Assessment___4
 {
     internal class Program
     {
-
+        //Exercise #2
         //static void GetMax(int number1, int number2)
         //{
 
@@ -28,57 +29,132 @@ namespace Self_Assessment___4
         //}
 
 
-        static void Main(string[] args)
+        //Exercise #3
+
+        //static void Main(string[] args)
+        //{
+        //    string inputNumber = Console.ReadLine();
+        //    Console.WriteLine(EnglishName(inputNumber));
+        //}
+
+        //static string EnglishName(string number)
+        //{
+        //    Console.WriteLine("Please enter a number:");
+
+        //    int lastDigit = number[number.Length - 1];
+        //    string englishDigit = string.Empty;
+
+        //    switch (lastDigit)
+        //    {
+        //        case '0':
+        //            englishDigit = "zero"; 
+        //            break;
+
+        //        case '1':
+        //            englishDigit = "one";
+        //            break;
+        //        case '2':
+        //            englishDigit = "two";
+        //            break;
+        //        case '3':
+        //            englishDigit = "three";
+        //            break;
+        //        case '4':
+        //            englishDigit = "four";
+        //            break;
+        //        case '5':
+        //            englishDigit = "five";
+        //            break;
+        //        case '6':
+        //            englishDigit = "six";
+        //            break;
+        //        case '7':
+        //            englishDigit = "seven";
+        //            break;
+        //        case '8':
+        //            englishDigit = "eight";
+        //            break;
+        //        case '9':
+        //            englishDigit = "nine";
+        //            break;
+        //    }
+        //    return englishDigit;
+        //}
+
+        //Exercise #6
+
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Please input numbers.");
+        //    int sizeArray1 = Convert.ToInt32(Console.ReadLine());
+        //    int[] Array1 = new int[sizeArray1];
+
+        //}
+
+        //static int FirstOccurence(int firstElement)
+        //    foreach(int Array1 in array)
+        //    {
+        //    if array==array+1
+        //    }
+
+        //Exercise #11
+
+        static void main(string[] args)
         {
-            string inputNumber = Console.ReadLine();
-            Console.WriteLine(EnglishName(inputNumber));
+            ThreeTasks();
         }
-
-        static string EnglishName(string number)
+        static int ThreeTasks(int number)
         {
-            Console.WriteLine("Please enter a number:");
-            
-            int lastDigit = number[number.Length - 1];
-            string englishDigit = string.Empty;
+            Console.WriteLine("Please select a task:");
+            Console.WriteLine("1) Reverse Order");
+            Console.WriteLine("2) Average");
+            Console.WriteLine("3) Solve Linear Equation");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input numbers.");
+            int sizearray1 = Convert.ToInt32(Console.ReadLine());
 
-            switch (lastDigit)
+            switch (answer)
             {
-                case '0':
-                    englishDigit = "zero"; 
-                    break;
+                case 1:
                     
-                case '1':
-                    englishDigit = "one";
+                    int[] array1 = new int[sizearray1];
+                    int reverseOrder = Array.Reverse(array1);
+                    Console.WriteLine(reverseOrder);
                     break;
-                case '2':
-                    englishDigit = "two";
+                case 2:
+                    
+                    int[] array2 = new int[sizearray1];
+                    int sum = array2.Sum();
+                    int length = array2.Length;
+                    int average = sum / length;
+                    Console.WriteLine(average);
                     break;
-                case '3':
-                    englishDigit = "three";
+                case 3:
+                    Console.WriteLine("Please input a:");
+                    int a = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please input b:");
+                    int b = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please input x:");
+                    int x = Convert.ToInt32(Console.ReadLine());
+
+                    if (a == 0)
+                    {
+                        if (b == 0)
+                            Console.WriteLine("Unlimited result equation");
+                        else
+                            Console.WriteLine("No result");
+                    }
+                    else
+                    {
+                        x = -b / a;
+                        Console.WriteLine("1 result");
+                    }
                     break;
-                case '4':
-                    englishDigit = "four";
-                    break;
-                case '5':
-                    englishDigit = "five";
-                    break;
-                case '6':
-                    englishDigit = "six";
-                    break;
-                case '7':
-                    englishDigit = "seven";
-                    break;
-                case '8':
-                    englishDigit = "eight";
-                    break;
-                case '9':
-                    englishDigit = "nine";
-                    break;
+                    //Solve the linear equation a * x + b = 0
+
+
+
             }
-            return englishDigit;
         }
-
-       
-
     }
 }
